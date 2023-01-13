21a3e9b47350d65466d3e8e7a53d2368710f2a0f
Mouhamad Kamaredine <108958123+Mouhamadkamaredine@users.noreply.github.com>
2023-01-13 04:10:38 +0200

    Delete KamaredineHotel directory

--- KamaredineHotel/src/app/Shared/Booking.ts
+++
@@ -1,6 +0,0 @@
-export class Book2 {
-  bookingId: number = 0;
-  checkin: string = '';
-  checkout: string = '';
-  roomNb: number = 0;
-}
--- KamaredineHotel/src/app/Shared/Facility.ts
+++
@@ -1,6 +0,0 @@
-export class Facility {
-  id: number = 0;
- name: string = '';
-  title: string = '';
-  image: string = '';
-}
